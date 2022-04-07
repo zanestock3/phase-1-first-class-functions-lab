ea748db75ae07648e1036a56371d8553a59f65e3
Zane Stock <zanestock3@gmail.com>
2022-04-07 07:37:26 -0500

    commit tree e684a5c0b7bcb47af15ea68978987d4e2f3b2cea

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array) {
+    return [...array.slice(0, 2)]
+}
+
+const returnLastTwoDrivers = function(array) {
+    return [...array.slice(2)]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(abc) {
+    return function() {
+            return (abc * 5)
+    }
+}
+
+const fareDoubler = function(abc) {
+    return abc * 2
+}
+const fareTripler = function (abc) {
+    return abc * 3
+}
+
+const selectDifferentDrivers = function (returnLastTwoDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers()
+}
